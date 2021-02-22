f020f7192d0dbc8045e307a17d7efe5f09d2f3fd
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-02-22 09:44:04 +0100

    viewport

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,34 @@
-$(function() {
-  $.scrollify({
-		section:".scrollable-section",
-    interstitialSection : "",
-    easing: "easeOutExpo",
-    scrollSpeed: 500,
-    offset : 30,
-    scrollbars: true,
-    standardScrollElements: "",
-    setHeights: false,
-    overflowScroll: true,
-    updateHash: true,
-    touchScroll:true,
-    before:function(){
+if(!(/Android|webOS|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i.test(navigator.userAgent) )) {
+  $(function() {
+    $.scrollify({
+  		section:".scrollable-section",
+      interstitialSection : "",
+      easing: "easeOutExpo",
+      scrollSpeed: 500,
+      offset : 30,
+      scrollbars: true,
+      standardScrollElements: "",
+      setHeights: false,
+      overflowScroll: true,
+      updateHash: true,
+      touchScroll:true,
+      before:function(){
+        $(".pagination .active").removeClass("active");
+      },
+
+      after:function(){
+        var panelNumber = $.scrollify.current().attr("data-panel");
+        $("#"+panelNumber).addClass("active");
+      },
+    });
+
+    $(".pagination a").on("click",function() {
+      $.scrollify.move($(this).attr("href"));
       $(".pagination .active").removeClass("active");
-    },
+      $(this).addClass("active");
+    });
 
-    after:function(){
-      var panelNumber = $.scrollify.current().attr("data-panel");
-      $("#"+panelNumber).addClass("active");
-    },
+
+
   });
-
-  $(".pagination a").on("click",function() {
-    $.scrollify.move($(this).attr("href"));
-    $(".pagination .active").removeClass("active");
-    $(this).addClass("active");
-  });
-
-
-
-});
+}
